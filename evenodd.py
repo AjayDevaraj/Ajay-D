63bf2e5856f6f78d88784cf47a669855c4ece8bf
AjayDevaraj <36471719+AjayDevaraj@users.noreply.github.com>
2018-02-19 11:52:19 +0000

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,9 @@
+def main():
+    pass
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+if __name__ == '__main__':
+    main()
