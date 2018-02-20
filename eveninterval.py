ddfbcb187b1ad442524e451f48f5e8cd0944be49
AjayDevaraj <36471719+AjayDevaraj@users.noreply.github.com>
2018-02-20 17:39:04 +0530

    Create eveninterval.py

---
+++ eveninterval.py
@@ -0,0 +1,9 @@
+def main():
+    pass
+lower_limit = int(input("Enter the lower limit : "))
+upper_limit = int(input("Enter the upper limit : "))
+for i in range(lower_limit,upper_limit+1):
+  if(i%2 == 0):
+    print(i)
+if __name__ == '__main__':
+    main()
