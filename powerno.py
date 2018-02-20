4caf0e3f6842c219224e2655a5ebec086bf7858c
AjayDevaraj <36471719+AjayDevaraj@users.noreply.github.com>
2018-02-20 17:26:34 +0530

    Create powerno.py

---
+++ powerno.py
@@ -0,0 +1,12 @@
+def main():
+    pass
+def power(base,exp):
+    if(exp==1):
+        return(base)
+    if(exp!=1):
+        return(base*power(base,exp-1))
+base=int(input("Enter base: "))
+exp=int(input("Enter exponential value: "))
+print("Result:",power(base,exp))
+if __name__ == '__main__':
+    main()
